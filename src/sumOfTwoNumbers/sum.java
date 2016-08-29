49e9f29e3a3de921d923cf40a42837eeebf6bc41
mutyalashailaja <mutyala.shailaja@gmail.com>
2016-08-29 16:40:54 +0530

    sesssion1assignment1

---
+++ src/sumOfTwoNumbers/sum.java
@@ -0,0 +1,17 @@
+package sumOfTwoNumbers;
+
+public class sum {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		 double b=2.5;
+		 double c;
+            c=a+b;
+            System.out.println("sum of a and b is :"+c);
+
+
+	}
+
+}
